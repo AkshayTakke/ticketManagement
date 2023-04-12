29ac7852cbc57592db00e7ad94b5d2f9bd430b27
Keyur Prajapati <129269391+keyur97@users.noreply.github.com>
2023-04-13 00:13:07 +0100

    added homepage support from backend

--- controller.py
+++ controller.py
@@ -1,1 +1,8 @@
+from flask import Flask,render_template,request,session,make_response,redirect,url_for
+from db_config import mysql
+from flask_restful import Resource
+import pymysql,sys
 
+class homePage(Resource):
+    def get(self):
+        return redirect(url_for('login'))
