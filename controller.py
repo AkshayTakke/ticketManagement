40628edd61f819257fef966f7ddafdbbc47ef404
Keyur Prajapati <129269391+keyur97@users.noreply.github.com>
2023-04-14 03:09:06 +0100

    Login Backend added

--- controller.py
+++ controller.py
@@ -23,3 +23,37 @@ class dashboard(Resource):
             return make_response(render_template('dashboard.html',countActive = activeCount,countClose = closeCount))
         else:
             return make_response(render_template('login.html'))
+      
+    
+class loginAPI(Resource):
+    def post(self):
+        try:
+            if 'username' in request.form and 'password' in request.form:
+                username = request.form['username']
+                password = request.form['password']
+                conn = mysql.connect()
+                cur = conn.cursor(pymysql.cursors.DictCursor)
+                cur.execute("select * from users where username = %s and password = %s",(username,password))
+                userData = cur.fetchone()
+                cur.close()
+                if userData:
+                    session['loggedin'] = True
+                    session['username'] = userData['username']
+                    message = "Logged In successfully"
+                    return redirect(url_for('dashboard'))
+                else:
+                    message = "Please enter correct username/password"
+                    return redirect(url_for('login',message =message))
+            else:
+                return redirect(url_for('login'))
+        except Exception as e:
+            print(e)
+    def get(self):
+        try:
+            if request.args.get('message'):
+                message = request.args.get('message')
+                return make_response(render_template('login.html',message =message))
+            else:    
+                return make_response(render_template('login.html'))
+        except Exception as e:
+            print(e)
