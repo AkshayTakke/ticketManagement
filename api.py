2b6d71a925971b00f21a121d1a2a75f9a89a790f
AkshayTakke <54357275+AkshayTakke@users.noreply.github.com>
2023-04-16 00:22:48 +0100

    fixed endpoint issues

--- api.py
+++ api.py
@@ -6,3 +6,5 @@ def routes(api):
     api.add_resource(logout, '/logout')
     api.add_resource(homePage, '/')
     api.add_resource(tickets, '/tickets',methods = ['GET','POST','DELETE'])
+    api.add_resource(createTicket, '/create_ticket',endpoint='create_ticket')
+    api.add_resource(dashboard,'/dashboard',endpoint = 'dashboard')
