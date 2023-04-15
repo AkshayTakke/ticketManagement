ddfdcc7f6f44e8e60e030d4c4c3826f8f27895a0
Ved1221 <104488973+Ved1221@users.noreply.github.com>
2023-04-15 02:33:22 +0100

    dashboard api added

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
-from controller import loginAPI,logout,homePage
+from controller import loginAPI,logout,homePage,dashboard
 
 
 def routes(api):
     api.add_resource(loginAPI, '/login',endpoint= 'login')
     api.add_resource(logout, '/logout')
     api.add_resource(homePage, '/')
+    api.add_resource(dashboard,'/dashboard',endpoint = 'dashboard')
