8a77c6c22615ec20abcf9ea23bfeef08f79c33c2
ujwalakabadi0508 <58591291+ujwalakabadi0508@users.noreply.github.com>
2023-04-15 21:04:28 +0100

    added the methods in app.py

--- api.py
+++ api.py
@@ -5,3 +5,4 @@ def routes(api):
     api.add_resource(loginAPI, '/login',endpoint= 'login')
     api.add_resource(logout, '/logout')
     api.add_resource(homePage, '/')
+    api.add_resource(tickets, '/tickets',methods = ['GET','POST','DELETE'])
