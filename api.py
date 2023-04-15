7e69286eec90418ec5deea53f7898d4ead330759
Keyur Prajapati <129269391+keyur97@users.noreply.github.com>
2023-04-15 02:18:23 +0100

    update login and logout api

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-from controller import loginAPI
+from controller import loginAPI,logoutx 
 
 
 def routes(api):
     api.add_resource(loginAPI, '/login',endpoint= 'login')
+    api.add_resource(logout, '/logout')
